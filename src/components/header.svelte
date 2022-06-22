4f98eb0a3cfe4957c6badbc4227675cd785add91
zim <webmaster@coolguy.website>
2022-06-22 13:41:29 +1200

    Add layout for routes with template header

---
+++ src/components/header.svelte
@@ -0,0 +1,41 @@
+<html>
+  <header>
+    <h1><a href="/">Ten Bodies</a></h1>
+    <nav>
+      <li><a href="/jewelry">jewelry</a></li>
+      <li><a href="/art">art</a></li>
+      <li><a href="/music">music</a></li>
+      <li><a href="/yoga">yoga</a></li>
+      <li><a href="/etc">etc</a></li>
+    </nav>
+  </header>
+</html>
+
+<style>
+ h1 {
+   text-align: center;
+   margin: 0;
+   padding: 0;
+ }
+ nav {
+   display: flex;
+   justify-content: center;
+   align-items: center;
+ }
+
+ li {
+   margin: var(--space-xs);
+ }
+
+ a {
+   color: inherit;
+   text-decoration: none;
+ }
+
+ li {
+   color: var(--colour-accent);
+ }
+ li a:hover {
+   color: var(--colour-body);
+ }
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<script>
+ import Header from '../components/header.svelte'
+</script>
+<main>
+<Header />
+<main>
+  <slot />
+</main>
+</main>
