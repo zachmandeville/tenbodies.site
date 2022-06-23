92234b078430e719aa861bed18abc62c66e3de87
zim <webmaster@coolguy.website>
2022-06-23 23:23:48 +1200

    add shopify queries

---
+++ src/queries/index.js
@@ -0,0 +1,87 @@
+export const getAllProducts = `
+query GetAllProducts {
+         products(sortKey: TITLE, first: 100) {
+          edges {
+            node {
+              id
+              handle
+              description
+              title
+              totalInventory
+              productType
+              variants(first: 5) {
+                edges {
+                  node {
+                    id
+                    title
+                    quantityAvailable
+                    price
+                  }
+                }
+              }
+              priceRange {
+                maxVariantPrice {
+                  amount
+                  currencyCode
+                }
+                minVariantPrice {
+                  amount
+                  currencyCode
+                }
+              }
+              images(first: 1) {
+                edges {
+                  node {
+                    src
+                    altText
+                  }
+                }
+              }
+            }
+          }
+        }
+    }
+`
+
+export const getProduct = `
+query getProduct($handle: String!) {
+          productByHandle(handle: $handle) {
+            id
+            handle
+            description
+            title
+            totalInventory
+            variants(first: 5) {
+              edges {
+                node {
+                  id
+                  title
+                  quantityAvailable
+                  priceV2 {
+                    amount
+                    currencyCode
+                  }
+                }
+              }
+            }
+            priceRange {
+              maxVariantPrice {
+                amount
+                currencyCode
+              }
+              minVariantPrice {
+                amount
+                currencyCode
+              }
+            }
+            images(first: 1) {
+              edges {
+                node {
+                  src
+                  altText
+                }
+              }
+            }
+          }
+        }
+`
