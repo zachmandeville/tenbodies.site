0f4779ee94fadc914dd9e788f26db509bede16b1
zim <webmaster@coolguy.website>
2022-06-23 23:47:33 +1200

    add markdown support

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,12 @@
 import adapter from '@sveltejs/adapter-auto';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+    extensions: ['.svelte','.md'],
+    preprocess: [
+        mdsvex({extensions: ['md']})
+    ],
 	kit: {
 		adapter: adapter()
 	}
